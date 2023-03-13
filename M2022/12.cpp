e21cb1a971265fd541f1dbf879299893e659c949
Klara1423 <2218278697@qq.com>
2023-03-13 23:28:08 +0800

    补题

--- M2022/12.cpp
+++ M2022/12.cpp
@@ -1,22 +1,49 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
+#include <vector>
 using namespace std;
-const int N = 1e5 + 10;
+const int N = 1e5 + 5;
 int main()
 {
-    int n = 0, m = 0, z = 1, a[N][N] = {0}, t[105] = {0};
+    int n = 0, m = 0, z = 1, t[105] = {0};
+    vector<vector<int>>a;
+    vector<int>b;
+    a.push_back(b);
     cin >> n >> m;
 
     for (int i = 1; i <= n; i++)
     {
-        cin >> a[i][0];
-        for (int j = 1; j <= a[i][0]; j++)
+        
+        int k = 0;
+        cin >> k;
+        b.push_back(k);
+
+        for (int j = 1; j <= b[0]; j++)
         {
-            cin >> a[i][j];
+            cin >> k;
+            b.push_back(k);
         }
+
+        a.push_back(b);
+        b.clear();
     }
-    
+
+    // vector<int>a[N];
+    // int n = 0, m = 0, z = 1, t[105] = {0};
+    // cin >> n >> m;
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     int k = 0;
+    //     cin >> k;
+    //     a[i].push_back(k);
+    //     for (int j = 1; j <= a[i][0]; j++)
+    //     {
+    //         cin >> k;
+    //         a[i].push_back(k);
+    //     }
+    // }
+
+
     for (int i = 0; i < m; i++)
     {
         int x = 0, y = 0;
@@ -36,7 +63,7 @@ int main()
         }
         // cout << i + 1 << ":" << z << endl;  
     }
-    
+
     cout << z << endl ;
     return 0;
 }
